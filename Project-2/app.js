8b49049354fa649e090583df0200e45cbb645d2f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-19 22:07:02 +0430

    set up all get elements

--- Project-2/app.js
+++ Project-2/app.js
@@ -7,3 +7,19 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  this.list = [...element.querySelectorAll(".img")]
+
+  // target
+
+  this.modal = getElement(".modal")
+  this.modalImg = getElement(".main-img")
+  this.modalImges = getElement(".modal-images")
+  this.closeBtn = getElement(".close-btn")
+  this.prevBtn = getElement(".prev-btn")
+  this.nextBtn = getElement(".next-btn")
+}
+
+const nature = new Gallery(getElement(".nature"))
+const city = new Gallery(getElement(".city"))
