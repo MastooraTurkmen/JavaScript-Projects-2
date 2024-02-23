5663c82d4b874a1ddeca2f64a42099fce6ed1161
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-23 15:55:37 +0430

    set up

--- Project-3: Jokes/app.js
+++ Project-3: Jokes/app.js
@@ -10,4 +10,16 @@ function getData(url) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.send();
-}
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) return;
+        if (xhr.readyState === 200) {
+            const { value: joke } = JSON.parse(xhr.responseText);
+            content.textContent = joke;
+        } else {
+            console.log({
+                status: xhr.status,
+                text: xhr.statusText,
+            });
+        };
+    };
+};
