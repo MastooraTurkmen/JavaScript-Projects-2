18ede450b92865597ca6d40e428ee279dd5b5438
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-23 15:50:54 +0430

    set up

--- Project-3: Jokes/app.js
+++ Project-3: Jokes/app.js
@@ -3,6 +3,10 @@ const URL = "https://api.chucknorris.io/jokes/random";
 const content = document.querySelector(".content")
 
 btn.addEventListener("click", function () {
-
 })
 
+function getData(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+}
