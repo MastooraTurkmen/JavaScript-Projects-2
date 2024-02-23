3e4fdd936618a630cb5054dc375c6efd3a9e881f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-23 15:50:29 +0430

    set up

---
+++ Project-3: Jokes/app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn");
+const URL = "https://api.chucknorris.io/jokes/random";
+const content = document.querySelector(".content")
+
+btn.addEventListener("click", function () {
+
+})
+
