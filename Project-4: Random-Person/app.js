79259b65b61a7e89ce635cd35f4c3e239fb553da
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-24 20:53:41 +0430

    set up elements

---
+++ Project-4: Random-Person/app.js
@@ -0,0 +1,8 @@
+import getElement from "./utils/getElement"
+
+const img = getElement(".user-img");
+const title = getElement(".user-title");
+const value = getElement(".user-value");
+const btn = getElement(".btn");
+const btns = [...document.querySelectorAll(".icon")];
+
