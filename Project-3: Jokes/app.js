20ee9c5301578eab27461c9a5ed4ad77df0860ea
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-24 19:23:39 +0430

    sloving js with fetch

--- Project-3: Jokes/app.js
+++ Project-3: Jokes/app.js
@@ -5,38 +5,18 @@ const img = document.querySelector(".container img");
 const URL = "https://api.chucknorris.io/jokes/random";
 
 btn.addEventListener("click", () => {
-    getData(URL)
-        .then(respons => displayData(respons))
-        .catch(error => console.log(error))
-})
+    fetch(URL)
+        .then(respons => respons.json())
+        .then(data => displayData(data))
+});
 
-function getData(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.send();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState !== 4) return;
-            if (xhr.status === 200) {
-                resolve(xhr.responseText)
-            } else {
-                reject({
-                    status: xhr.status,
-                    text: xhr.statusText,
-                })
-            };
-        };
-    })
-};
-
-
-function displayData() {
+function displayData(data) {
     img.classList.add("shake-img");
-    const { value: joke } = JSON.parse(xhr.responseText);
+    const { value: joke } = data;
     content.textContent = joke;
     const random = Math.random() * 1000;
 
     setTimeout(() => {
         img.classList.remove("shake-img")
     }, random)
-}
+};
