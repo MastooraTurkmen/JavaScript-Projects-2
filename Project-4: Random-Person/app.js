6546d651ea3e33bcb2f1c6e2a0b959c3da320c6a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-25 08:07:57 +0430

    app js showUser

--- Project-4: Random-Person/app.js
+++ Project-4: Random-Person/app.js
@@ -1,4 +1,5 @@
-import getElement from "./utils/getElement"
+import getElement from "./utils/getElement";
+import getUser from "./utils/getUser";
 
 const img = getElement(".user-img");
 const title = getElement(".user-title");
@@ -6,3 +7,11 @@ const value = getElement(".user-value");
 const btn = getElement(".btn");
 const btns = [...document.querySelectorAll(".icon")];
 
+
+const showUser = async () => {
+    const person = await getUser()
+    console.log(person)
+}
+
+
+window.addEventListener("DOMContentLoaded", showUser)
