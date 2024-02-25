8311e5ab6aea13af0e5ed695c5919ee2a12028b2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-25 08:52:12 +0430

    get Element

--- Project-4: Random-Person/utils/getElement.js
+++ Project-4: Random-Person/utils/getElement.js
@@ -1,4 +1,4 @@
-export default getElement = (selection) => {
+export default selection => {
     const element = document.querySelector(selection);
     if (element) return element;
     throw new Error(`There is no such an ${element}`);
