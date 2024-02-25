ab17fd034dd4507cf65c476311195847bf676490
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-25 08:52:17 +0430

    get User

--- Project-4: Random-Person/utils/getUser.js
+++ Project-4: Random-Person/utils/getUser.js
@@ -1,7 +1,26 @@
+const URL = "https://randomuser.me/api/";
+
 const getUser = async () => {
-    const response = await fetch(URL)
+    const response = await fetch(URL);
+    const data = await response.json();
+    const person = data.results[0];
+    const { phone, email } = person;
+    const { large: image } = person.picture;
+    const { password } = person.login;
+    const { first, last } = person.name;
+    const { dob: { age } } = person;
+    const { street: { number, name } } = person.location;
+
+    return {
+        image,
+        phone,
+        email,
+        age,
+        password,
+        street: `${number} ${name}`,
+        name: `${first} ${last}`
+    }
+
 };
-
-
 
 export default getUser;
