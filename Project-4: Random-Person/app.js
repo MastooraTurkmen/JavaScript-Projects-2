947792d74b6879e1631e2d5c8260b41c00932ef6
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-25 09:53:49 +0430

    add click

--- Project-4: Random-Person/app.js
+++ Project-4: Random-Person/app.js
@@ -1,18 +1,15 @@
 import getElement from "./utils/getElement.js";
 import getUser from "./utils/getUser.js";
+import displayUser from "./utils/displayUser.js";
 
-const img = getElement(".user-img");
-const title = getElement(".user-title");
-const value = getElement(".user-value");
 const btn = getElement(".btn");
 const btns = [...document.querySelectorAll(".icon")];
 
-
 const showUser = async () => {
     const person = await getUser()
-    console.log(person)
+    displayUser(person)
 }
 
 
 window.addEventListener("DOMContentLoaded", showUser);
-btn.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
