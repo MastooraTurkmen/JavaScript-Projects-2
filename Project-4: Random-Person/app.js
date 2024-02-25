9b3214efa89f65881693011048ff1c7262606ea0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-25 10:10:06 +0430

    set up appjs

--- Project-4: Random-Person/app.js
+++ Project-4: Random-Person/app.js
@@ -3,13 +3,11 @@ import getUser from "./utils/getUser.js";
 import displayUser from "./utils/displayUser.js";
 
 const btn = getElement(".btn");
-const btns = [...document.querySelectorAll(".icon")];
 
 const showUser = async () => {
     const person = await getUser()
     displayUser(person)
 }
-
 
 window.addEventListener("DOMContentLoaded", showUser);
 btn.addEventListener("click", showUser);
