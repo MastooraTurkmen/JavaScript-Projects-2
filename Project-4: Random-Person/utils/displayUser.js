c08ba0374bff1f2bd9e090da62fa7ad31183f408
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-25 10:08:54 +0430

    adds forEach for btns

--- Project-4: Random-Person/utils/displayUser.js
+++ Project-4: Random-Person/utils/displayUser.js
@@ -1,5 +1,6 @@
 import getElement from "./getElement.js";
 
+const btns = [...document.querySelectorAll(".icon")];
 const img = getElement(".user-img");
 const title = getElement(".user-title");
 const value = getElement(".user-value");
@@ -7,7 +8,19 @@ const value = getElement(".user-value");
 function displayUser(person) {
     img.src = person.image;
     value.textContent = person.name;
-    title.textContent = 'My Name is';
+    title.textContent = 'My name is';
+    btns.forEach((btn)=> btn.classList.remove("active"))
+    btns[0].classList.add("active");
+
+    btns.forEach((btn) => {
+        const label = btn.dataset.label;
+        btn.addEventListener('click', () => {
+            value.textContent = person[label];
+            title.textContent = `My ${label} is`;
+            btns.forEach((btn) => btn.classList.remove("active"));
+            btn.classList.add("active");
+        })
+    })
 
 }
 
