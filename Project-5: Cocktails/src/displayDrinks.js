1e39a207a5ee3f0d04100e8304ffb04c4771a66f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-26 08:54:43 +0430

    display drinks

--- Project-5: Cocktails/src/displayDrinks.js
+++ Project-5: Cocktails/src/displayDrinks.js
@@ -1,5 +1,28 @@
+import getElemet from './getElement';
 
+export default function displayDrinks({drinks}) {
+    const section = getElemet('.section-center');
+    const title = getElemet('.title');
 
-export default function displayDrinks() {
-    
+    if (!drinks) {
+        title.textContent = "Sorry, No Drinks finds :(";
+        section.innerHTML = null;
+        return
+    }
+
+    const newDrink = drinks.map((drink) => {
+        const {idDrink: id, strDrink: name, strDrinkThumb:image} = drink
+        return `
+        <a href="drink.html">
+        <article class="cocktail" data-id="${id}">
+          <img src="${image}" alt="${name}">
+          <h3>${name}</h3>
+        </article>
+      </a>
+        `
+    }).join('')
+
+    title.textContent = "";
+    section.innerHTML = newDrink;
+    return section;
 }
