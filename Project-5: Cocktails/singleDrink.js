d787b9172c1d62c4643ce8ad8dc11f6f815954c3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-26 19:20:57 +0430

    set present Drink

--- Project-5: Cocktails/singleDrink.js
+++ Project-5: Cocktails/singleDrink.js
@@ -2,9 +2,16 @@ import fetchDrink from "./src/fetchDrinks.js";
 import displayDrink from "./src/displaySingleDrink.js";
 
 const presentDrink = async () => {
+
     const id = localStorage.getItem("drink");
-    const drink = await fetchDrink(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${id}`);
-    displayDrink(drink)
+    if (!id) {
+        window.location.replace("index.html");
+    } else {
+        const drink = await fetchDrink(
+            `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${id}`
+        );
+        displayDrink(drink)
+    }
 }
 
 window.addEventListener("DOMContentLoaded", presentDrink)
