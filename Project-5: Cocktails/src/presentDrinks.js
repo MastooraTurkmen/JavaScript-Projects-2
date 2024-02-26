f9c035888c56071651d9271dcf8714ec3c4316c2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-26 19:40:48 +0430

    present drinks

--- Project-5: Cocktails/src/presentDrinks.js
+++ Project-5: Cocktails/src/presentDrinks.js
@@ -1,14 +1,17 @@
-import fetchDrink from "./fetchDrinks.js";
-import displayDrinks from "./displayDrinks.js";
-import setDrink from "./setDrink.js";
+import fetchDrinks from './fetchDrinks.js';
+import displayDrinks from './displayDrinks.js';
+import setDrink from './setDrink.js';
+
 
 const showDrinks = async (url) => {
-    const data = await fetchDrink(url);
-    const section = await displayDrinks(data);
+    // fetch drinks
+    const data = await fetchDrinks(url);
 
+    // display drinks
+    const section = await displayDrinks(data);
     if (section) {
-        setDrink(section)
+        setDrink(section);
     }
-}
+};
 
 export default showDrinks;
