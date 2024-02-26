7a1197a71fb34ea249e8f46b4717e32c983d6a83
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-26 08:29:28 +0430

    fetch drinks

---
+++ Project-5: Cocktails/src/fetchDrinks.js
@@ -0,0 +1,12 @@
+
+const fetchDrink = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default fetchDrink;
