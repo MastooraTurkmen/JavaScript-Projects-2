ce4dff51877dc086d98ea3e97f3d564ea21a4529
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-26 19:40:23 +0430

    dinks display

--- Project-5: Cocktails/src/displayDrinks.js
+++ Project-5: Cocktails/src/displayDrinks.js
@@ -1,28 +1,31 @@
-import getElemet from './getElement';
+import get from './getElement.js';
+import { hideLoading } from './toggleLoading.js';
+const displayDrinks = ({ drinks }) => {
+  const section = get('.section-center');
+  const title = get('.title');
+  if (!drinks) {
+    // hide loading
+    hideLoading();
+    title.textContent = 'sorry, no drinks matched your search';
+    section.innerHTML = null;
+    return;
+  }
+  const newDrinks = drinks
+    .map((drink) => {
+      const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
 
-export default function displayDrinks({drinks}) {
-    const section = getElemet('.section-center');
-    const title = getElemet('.title');
+      return `<a href="drink.html">
+          <article class="cocktail" data-id="${id}">
+            <img src="${image}" alt="${name}" />
+            <h3>${name}</h3>
+          </article>
+        </a>`;
+    })
+    .join('');
+  hideLoading();
+  title.textContent = '';
+  section.innerHTML = newDrinks;
+  return section;
+};
 
-    if (!drinks) {
-        title.textContent = "Sorry, No Drinks finds :(";
-        section.innerHTML = null;
-        return
-    }
-
-    const newDrink = drinks.map((drink) => {
-        const {idDrink: id, strDrink: name, strDrinkThumb:image} = drink
-        return `
-        <a href="drink.html">
-        <article class="cocktail" data-id="${id}">
-          <img src="${image}" alt="${name}">
-          <h3>${name}</h3>
-        </article>
-      </a>
-        `
-    }).join('')
-
-    title.textContent = "";
-    section.innerHTML = newDrink;
-    return section;
-}
+export default displayDrinks;
