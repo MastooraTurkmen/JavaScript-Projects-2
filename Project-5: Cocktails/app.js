72d325948db1897551df62a29eea6edc1146bb56
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-26 19:40:09 +0430

    set up

--- Project-5: Cocktails/app.js
+++ Project-5: Cocktails/app.js
@@ -1,7 +1,7 @@
-import showDrinks from "./src/setDrink.js"
+import presentDrinks from './src/presentDrinks.js';
+import './src/searchForm.js';
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 
-const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a"
-
-window.addEventListener("DOMContentLoaded", function () {
-    showDrinks(URL);
-})
+window.addEventListener('DOMContentLoaded', () => {
+    presentDrinks(URL);
+});
