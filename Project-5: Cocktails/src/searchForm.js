eacabe57b08cc03e5c7e9e02bab15a4d1bdcff81
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-26 19:40:58 +0430

    search form

--- Project-5: Cocktails/src/searchForm.js
+++ Project-5: Cocktails/src/searchForm.js
@@ -1,15 +1,14 @@
-import getElement from "./getElement.js";
-import presentDrink from "./presentDrinks.js";
+import get from './getElement.js';
+import presentDrinks from './presentDrinks.js';
 
-const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-const from = getElement(".search-from");
-const input = getElement('[name="drink"]');
+const form = get('.search-form');
+const input = get('[name="drink"]');
 
-
-from.addEventListener("keyup", function (e) {
+form.addEventListener('keyup', function (e) {
     e.preventDefault();
     const value = input.value;
     if (!value) return;
-    presentDrink(`${baseURL}${value}`);
+    presentDrinks(`${baseURL}${value}`);
 });
