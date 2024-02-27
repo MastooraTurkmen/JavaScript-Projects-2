034c6dc385e7ab9fef5dbd3a529d022703ff98ac
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 07:46:34 +0430

    set up

--- Project-6: Slider/app.js
+++ Project-6: Slider/app.js
@@ -4,3 +4,18 @@ const preBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const container = document.querySelector(".slide-container");
 
+container.innerHTML = people.map((item) => {
+    const { img, name, job, text } = item;
+
+    return `
+    <article class="slide">
+        <img src="${img}" class="img" alt="peter doe">
+        <h4>${name}</h4>
+        <p class="title">${job}</p>
+        <p class="text">${text}</p>
+        <div class="quote-icon">
+          <div class="fas fa-quote-right"></div>
+        </div>
+      </article>
+    `
+})
