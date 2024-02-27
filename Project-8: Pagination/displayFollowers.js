6434eb419b04bc91c8861785bcb3fdb59f91b6c1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 21:33:32 +0430

    map over followers

--- Project-8: Pagination/displayFollowers.js
+++ Project-8: Pagination/displayFollowers.js
@@ -1,6 +1,17 @@
 const container = document.querySelector(".container")
 
 const display = (followers) => {
+    const newFollowe = followers.map((follower) => {
+        const { avatar_url, html_url, login } = follower;
+        return `
+            <article class="card">
+                <img src="${avatar_url}" alt="${login}"/>{
+                <h4>${login}}</h4>
+                <a href="${html_url}" class="btn">view profile</a>
+            </article>
+        `;
+
+    }).join('')
 
 }
 
