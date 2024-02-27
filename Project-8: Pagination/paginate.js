425e18d018e8070b5315496d69836b869ad9ce4a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 22:09:37 +0430

    paginate

--- Project-8: Pagination/paginate.js
+++ Project-8: Pagination/paginate.js
@@ -2,7 +2,11 @@ const paginate = (followers) => {
     const itemOfPage = 10;
     const numberOfPage = Math.ceil(followers.length / itemOfPage);
 
-   
+    const newFollwers = Array.from(({length:numberOfPage}), (_, index) => {
+        const start = index * itemOfPage;
+        return followers.slice(start, start + itemOfPage);
+    })
+
 }
 
 export default paginate;
