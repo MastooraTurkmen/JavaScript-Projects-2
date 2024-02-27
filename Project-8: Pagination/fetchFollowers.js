bc1a6841ee7204f8001cd76bf7da6757f260ed3f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 21:11:45 +0430

    fetch data

--- Project-8: Pagination/fetchFollowers.js
+++ Project-8: Pagination/fetchFollowers.js
@@ -1,7 +1,8 @@
 const url = 'https://api.github.com/users/MastooraTurkmen/followers?per_page=100'
 
 const fetchFollowers = async () => {
-
+    const respons = await fetch(url);
+    
 }
 
 export default fetchFollowers
