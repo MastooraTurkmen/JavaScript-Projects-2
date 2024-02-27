665008e050ffe316072b2777980fb236ab2e7a8c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 21:12:59 +0430

    fetch the followers

--- Project-8: Pagination/app.js
+++ Project-8: Pagination/app.js
@@ -2,3 +2,9 @@ import fetchFollowers from './fetchFollowers.js'
 import displayFollowers from './displayFollowers.js'
 import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
+
+
+const init = async() => {
+    const followers = await fetchFollowers();
+    console.log(followers)
+}
