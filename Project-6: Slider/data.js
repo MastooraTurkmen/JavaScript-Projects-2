db8a723c5f3c777de7e618de2fbb5e77a745e539
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 15:30:49 +0430

    data js

--- Project-6: Slider/data.js
+++ Project-6: Slider/data.js
@@ -3,7 +3,8 @@ const people = [
     img: 'https://www.course-api.com/images/people/person-3.jpeg',
     name: 'peter doe',
     job: 'product manager',
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+    text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus quas voluptatum modi laborum ratione suscipit, voluptatibus voluptas aliquid quia possimus.
+ `,
   },
   {
     img: 'https://www.course-api.com/images/people/person-1.jpeg',
@@ -15,7 +16,7 @@ const people = [
     img: 'https://www.course-api.com/images/people/person-2.jpeg',
     name: 'emma doe',
     job: 'designer',
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    text: `Lorem ipsum dolor sit amet consectetur Natus quas voluptatum modi voluptatibus voluptas aliquid quia possimus. Quidem quoeius recusandae officia?`,
   },
 ];
 
