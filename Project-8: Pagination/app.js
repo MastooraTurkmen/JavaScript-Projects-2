ee9760596ad52e47ef668956dc83cd438e204ce2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 21:38:33 +0430

    add text content

--- Project-8: Pagination/app.js
+++ Project-8: Pagination/app.js
@@ -3,10 +3,12 @@ import displayFollowers from './displayFollowers.js'
 import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
 
+const text = document.querySelector('.section-title h1');
 
 const init = async () => {
     const followers = await fetchFollowers();
-    displayFollowers(followers)
+    displayFollowers(followers);
+    text.textContent = 'pagination';
 }
 
 window.addEventListener('load', init)
