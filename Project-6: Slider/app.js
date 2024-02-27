361d276adf1df6b856378a471f4193214c7831d2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-27 07:43:10 +0430

    import people js

--- Project-6: Slider/app.js
+++ Project-6: Slider/app.js
@@ -1,3 +1,5 @@
+import people from "./data.js";
+
 const preBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const container = document.querySelector(".slide-container");
