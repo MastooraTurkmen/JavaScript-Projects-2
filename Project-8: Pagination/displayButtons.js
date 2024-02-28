6951001c187af48523e975eaab11ccf6ab6be253
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-28 07:09:58 +0430

    display button

--- Project-8: Pagination/displayButtons.js
+++ Project-8: Pagination/displayButtons.js
@@ -1,3 +1,14 @@
-const displayButtons = () => {}
+const displayButtons = (container, pages, index) => {
+    let btns = pages.map((_, pageIndex) => {
+        return `
+             <button class="${index === pageIndex ? "active-btn" : "null"}"
+             data-index="${pageIndex}">${pageIndex + 1}</button>
+        `
+    })
+
+    btns.push(`<button class="next-btn">next</button>`);
+    btns.unshift(`<button class="prev-btn">prev</button>`);
+    container.innerHTML = btns.join('');
+}
 
 export default displayButtons
