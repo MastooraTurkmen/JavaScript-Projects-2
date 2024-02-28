6e266f0055691dd67f938616ca6f30856a7f3ac9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-28 07:09:52 +0430

    set up

--- Project-8: Pagination/app.js
+++ Project-8: Pagination/app.js
@@ -4,12 +4,21 @@ import paginate from './paginate.js'
 import displayButtons from './displayButtons.js'
 
 const text = document.querySelector('.section-title h1');
+const btnContainer = document.querySelector('.btn-container');
+
+let index = 0;
+let pages = [];
+
+const setUp = () => {
+    displayButtons(btnContainer, pages, index);
+    displayFollowers(pages[index]);
+};
 
 const init = async () => {
     const followers = await fetchFollowers();
-    displayFollowers(paginate(followers)[4]);
-    const page = paginate(followers)
     text.textContent = 'pagination';
-}
+    pages = paginate(followers);
+    setUp();
+};
 
 window.addEventListener('load', init)
