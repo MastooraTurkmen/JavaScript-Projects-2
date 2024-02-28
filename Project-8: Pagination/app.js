03abba05f0881e9ac844485c2e6d1c7796751aa1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-28 08:19:22 +0430

    set up app js

--- Project-8: Pagination/app.js
+++ Project-8: Pagination/app.js
@@ -21,4 +21,26 @@ const init = async () => {
     setUp();
 };
 
+btnContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains("btn-container")) return;
+    if (e.target.classList.contains("page-btn")) {
+        index = parseInt(e.target.dataset.index)
+    }
+    if (e.target.classList.contains("next-btn")) {
+        index++;
+        if (index > pages.length - 1) {
+            index = 0;
+        }
+    }
+
+    if (e.target.classList.contains("prev-btn")) {
+        index--;
+        if (index < 0) {
+            index = pages.length - 1
+        }
+    }
+
+    setUp()
+})
+
 window.addEventListener('load', init)
